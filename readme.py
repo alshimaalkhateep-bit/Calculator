a1d6ebecd49fae561bf58ad46af2ad22c36ed6a3
alshimaalkhateep-bit <alshimaalkhateep@gmail.com>
2026-01-28 11:40:29 +0200

    Add files via upload

---
+++ readme.py
@@ -0,0 +1,22 @@
+# =========================================================
+# Project Name: Simple Calculator (Addition)
+# Programming Language: Python
+#
+# Purpose of the Program:
+# This program defines a simple function that adds two numbers.
+#
+# What the program does:
+# - Takes two numbers as input (a and b)
+# - Returns the sum of these two numbers
+#
+# How this file is used:
+# - It can be imported into another file (Unit Test file)
+# - The function inside it can be tested automatically
+#
+# Expected Result:
+# - add(1, 2)  -> 3
+# - add(-1, 1) -> 0
+# - add(0, 0)  -> 0
+# =========================================================
+
+
